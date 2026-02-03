49ea67dae4f69933a4ad77bd7b9c07062b9f8c7c
Khalid Majeed <kmajeed85400@gmail.com>
2026-02-03 19:20:42 +0500

    Shop page changes

--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Shop = () => {
   return (
     <div>
-      Shop Page
+      <h2>Shop Page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis praesentium ex dolores ullam tempore laborum officia nihil. Libero maiores veniam, illo laudantium, mollitia repellat blanditiis consequatur voluptatibus ullam minima natus ad, recusandae quisquam nobis suscipit. Debitis aspernatur ratione ut quibusdam excepturi exercitationem! Deserunt, suscipit nobis recusandae harum voluptatibus esse magnam quod magni distinctio, eum inventore laboriosam, iste necessitatibus a voluptas? Vitae maxime incidunt provident temporibus? Ipsum adipisci commodi repellendus quas qui enim vitae, obcaecati quam magni quis dolorem. Architecto placeat, ipsum nobis cumque dolor nisi exercitationem soluta alias quidem adipisci mollitia modi vitae omnis, maxime eaque quas. Atque, quos dolores.
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex error magnam amet laudantium tempora excepturi obcaecati veritatis enim recusandae. Inventore praesentium aspernatur ea consequatur sit nihil laudantium corrupti tenetur debitis odio ducimus voluptate mollitia incidunt placeat, est iste sint quos modi repellat soluta dolorum autem, fuga quibusdam. Quia totam quisquam, alias amet aut ut pariatur consequuntur sed, voluptatum perferendis dolorem! Neque consequuntur quibusdam eius accusamus dolorum enim earum perspiciatis illum, impedit voluptatem corporis! Expedita ex voluptates neque corporis sint quos autem distinctio. Quam veritatis accusantium libero tenetur esse cum ab, odio assumenda inventore. Vero omnis nam nisi aspernatur provident, voluptatum hic, suscipit enim sint quaerat saepe, pariatur debitis impedit fugiat fugit aut sed itaque quasi animi doloribus vel. Alias, magnam odio illo numquam obcaecati perspiciatis non nobis perferendis, in tempore molestias id necessitatibus vitae ipsam soluta aperiam natus! Dolor excepturi minima cum non. Dicta, quisquam quas. Illum sint sed accusantium nulla adipisci quia tenetur corporis? Sapiente fuga fugiat exercitationem accusantium facere iure officiis quia provident, similique, ipsum pariatur! Odit adipisci debitis, ab totam tempore ad voluptate. Inventore suscipit consectetur necessitatibus itaque ab dolorem atque dolorum. Laborum soluta obcaecati esse neque, aspernatur ab omnis distinctio, ipsa ipsam cumque a dolores!
+      </p>
     </div>
   )
 }
