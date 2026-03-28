a5b372c7ce60083e109c22a923fa24d5c6c3a447
Khalid Majeed <kmajeed85400@gmail.com>
2026-03-28 22:31:51 +0500

    changed

--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -23,6 +23,7 @@ export const Shop = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, error. Qui modi voluptatem iusto nemo maiores! Qui, esse voluptatum! Ad!
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, iusto.
         <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam, quasi!
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, unde!
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, excepturi?
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum consectetur enim sequi a. Asperiores molestias vitae tempora esse iste! Asperiores odit quidem autem quasi cupiditate blanditiis molestias qui placeat. Ipsa.
